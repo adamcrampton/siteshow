8054882c4ccf4c8de0f0d78751e329a0dfee087c
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-08-10 17:04:36 +1000

    Added pages and config migrations

---
+++ database/migrations/2018_08_10_063430_create_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('url');
+            $table->tinyInteger('status');
+            $table->integer('duration');
+            $table->string('image_path');
+            $table->smallInteger('rank');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
