437b321a6abe8dbbd07928430b0cbde7e1168c58
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-09-09 18:11:11 +1000

    Setup validation rules setup method

--- app/Http/Controllers/ManagePagesController.php
+++ app/Http/Controllers/ManagePagesController.php
@@ -20,5 +20,35 @@ class ManagePagesController extends Controller
         $optionCollection->each(function($item, $key) {
             $this->globalOptions[$item->option_name] = $item->option_value;
         });
+
+        // Determine fields required for validation and associated rules.
+        $this->setValidationRules($this->controllerType);
+    }
+
+    private function setValidationRules($controllerType)
+    {
+        // Depending on the admin page, return the set of required validation rules.
+        switch ($controllerType) {
+            case 'option':
+                $this->insertValidationOptions = [];
+                $this->updateValidationOptions = [
+                    'global_delay' => 'required', 
+                    'global_fetch_limit' => 'required', 
+                    'global_fetch_delay' => 'required', 
+                    'global_page_list_limit' => 'required', 
+                    'user_agent' => 'required', 
+                    'default_save_path' => 'required',
+                    'global_overwrite_files' => 'required',
+                    'dismiss_dialogues' => 'required',
+                    'wait_until_network_idle' => 'required',
+                    'global_fetch_window_width' => 'required',
+                    'global_fetch_window_height' => 'required'
+                ];
+
+            default:
+                $this->insertValidationOptions = [];
+                $this->updateValidationOptions = [];
+                break;
+        }
     }
 }
