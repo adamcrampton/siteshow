42d88f9032c63a6dfd780f15e377bb8763605ba4
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-09-13 17:05:38 +1000

    Show page status, added toggle disabled button

--- resources/views/manage/page.blade.php
+++ resources/views/manage/page.blade.php
@@ -45,69 +45,75 @@
 	</div>
 </div>
 
-<h2 class="float-md-left">Manage Pages</h2>
-<div class="float-md-right">
-	<!-- Reserved for any needed right floated elements -->
-</div>
-{!! Form::open(['action' => ['PageController@batchUpdate'], 'class' => 'form', 'id' => 'update_form']) !!}
-<table class="table table-hover">
-	<thead>
-		<tr>
-			<th scope="col">Page Name</th>
-			<th scope="col">Page URL</th>
-			<th scope="col">Duration</th>
-			<th scope="col">Image Link</th>
-			<th scope="col">Rank</th>
-			<th scope="col">Status</th>
-			<th scope="col">Delete</th>
-		</tr>
-	</thead>
+<div class="col-lg-12 mt-5">
+	<h2 class="float-md-left">Manage Pages</h2>
+	<div class="float-md-right">
+		<a href="#" class="btn btn-primary btn-right">Show Disabled Pages</a>
+	</div>
+	{!! Form::open(['action' => ['PageController@batchUpdate'], 'class' => 'form', 'id' => 'update_form']) !!}
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th scope="col">Page Name</th>
+				<th scope="col">Page URL</th>
+				<th scope="col">Duration</th>
+				<th scope="col">Image Link</th>
+				<th scope="col">Rank</th>
+				<th scope="col">Status</th>
+				<th scope="col">Delete</th>
+			</tr>
+		</thead>
 
-	@foreach($page as $index => $pageValues)
-		<tr>
-			<td>
-				{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
-				{{ Form::text('page['. $index .'][id]', $pageValues->id, ['style' => 'display:none']) }}
-				{{ Form::text('page['. $index .'][original_value_name]', $pageValues->name, ['style' => 'display:none']) }}
-				{{ Form::text('page['. $index .'][name]', $pageValues->name, ['class' => 'form-control first_name', 'data-input-type' => 'name', 'data-update-row' => $index, 'required']) }}
-			</td>
-			<td>
-				{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
-				{{ Form::text('page['. $index .'][original_value_url]', $pageValues->url, ['style' => 'display:none']) }}
-				{{ Form::text('page['. $index .'][url]', $pageValues->url, ['class' => 'form-control last_name', 'data-input-type' => 'url', 'data-update-row' => $index, 'required']) }}
-			</td>
-			<td>
-				{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
-				{{ Form::text('page['. $index .'][original_value_duration]', $pageValues->duration, ['style' => 'display:none']) }}
-				{{ Form::text('page['. $index .'][duration]', $pageValues->duration, ['class' => 'form-control', 'id' => 'duration', 'required']) }}
-			</td>
-			<td>								
-				<a href="{{ $option['default_save_path'] }}{{ $pageValues->image_path }}.jpg">Link</a>
-			</td>
-			<td>								
-				{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
-				{{ Form::text('page['. $index .'][original_value_rank]', $pageValues->rank, ['style' => 'display:none']) }}
-				{{ Form::text('page['. $index .'][rank]', $pageValues->rank, ['class' => 'form-control', 'id' => 'rank', 'required']) }}
-			</td>
-			<td>								
-				TBA
-			</td>
-			<td class="text-center">
-				<div class="form-check">
-				{{-- There will either be a value or not in $_POST, so the actual value of the field set doesn't matter. --}}
-				{{ Form::checkbox('page['. $index .'][delete]', null, false, ['class' => 'form-check-input']) }}
-				</div>
-			</td>
-		</tr>
-	@endforeach
-		<tr>
-			<td colspan="7" class="text-right">
-				<input type="reset" class="btn btn-secondary" value="Cancel">
-				{!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
-			</td>
-		</tr>
-</table>
-{!! Form::close() !!}
+		@foreach($page as $index => $pageValues)
+			{{-- Hide row if item is disabled --}}
+			<tr {!! $pageValues->status == 0 ? 'class="d-none"' : '' !!}>
+				<td>
+					{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
+					{{ Form::text('page['. $index .'][id]', $pageValues->id, ['style' => 'display:none']) }}
+					{{ Form::text('page['. $index .'][original_value_name]', $pageValues->name, ['style' => 'display:none']) }}
+					{{ Form::text('page['. $index .'][name]', $pageValues->name, ['class' => 'form-control first_name', 'data-input-type' => 'name', 'data-update-row' => $index, 'required']) }}
+				</td>
+				<td>
+					{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
+					{{ Form::text('page['. $index .'][original_value_url]', $pageValues->url, ['style' => 'display:none']) }}
+					{{ Form::text('page['. $index .'][url]', $pageValues->url, ['class' => 'form-control last_name', 'data-input-type' => 'url', 'data-update-row' => $index, 'required']) }}
+				</td>
+				<td>
+					{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
+					{{ Form::text('page['. $index .'][original_value_duration]', $pageValues->duration, ['style' => 'display:none']) }}
+					{{ Form::text('page['. $index .'][duration]', $pageValues->duration, ['class' => 'form-control', 'id' => 'duration', 'required']) }}
+				</td>
+				<td>								
+					<a href="{{ $option['default_save_path'] }}{{ $pageValues->image_path }}.jpg">Link</a>
+				</td>
+				<td>								
+					{{-- Store id and original value for each row - to be processed as an array in the backend. --}}
+					{{ Form::text('page['. $index .'][original_value_rank]', $pageValues->rank, ['style' => 'display:none']) }}
+					{{ Form::text('page['. $index .'][rank]', $pageValues->rank, ['class' => 'form-control', 'id' => 'rank', 'required']) }}
+				</td>
+				<td>								
+					<select class="form-control" name="dismiss_dialogues" id="dismiss_dialogues" required>
+						<option value="1" {{ $pageValues->status == 1 ? 'selected' : '' }}>Active</option>
+						<option value="0" {{ $pageValues->status == 0 ? 'selected' : '' }}>Disabled</option>
+					</select>
+				</td>
+				<td class="text-center">
+					<div class="form-check">
+					{{-- There will either be a value or not in $_POST, so the actual value of the field set doesn't matter. --}}
+					{{ Form::checkbox('page['. $index .'][delete]', null, false, ['class' => 'form-check-input']) }}
+					</div>
+				</td>
+			</tr>
+		@endforeach
+			<tr>
+				<td colspan="7" class="text-right">
+					<input type="reset" class="btn btn-secondary" value="Cancel">
+					{!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
+				</td>
+			</tr>
+	</table>
+	{!! Form::close() !!}
+	</div>
 </div>
 
 @endsection
