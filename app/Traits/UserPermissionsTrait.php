eb1e11fb88bd3f33c49bcfa6e79ef7f22a02da3d
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-09-18 17:48:55 +1000

    Added helper functions in user permissions trait

--- app/Traits/UserPermissionsTrait.php
+++ app/Traits/UserPermissionsTrait.php
@@ -3,13 +3,36 @@
 namespace App\Traits;
 
 use App\Models\User;
+use Auth;
 
 trait UserPermissionsTrait
 {
     private $permissionLevels;
+    private $currentUserPermissionLevel;
 
     public function __construct()
     {
-        $this->permissionLevels = ['administrator', 'editor', 'viewer'];
+    	// Set the required permission membership for each level.
+        $this->permissionLevels['administrator'] = ['administrator'];
+        $this->permissionLevels['editor'] = ['administrator', 'editor'];
+        $this->permissionLevels['viewer'] = ['viewer', 'editor', 'viewer'];
+
+        // Get current user permission level.
+        $this->currentUserPermissionLevel = Auth::user()->permission->permission;
+    }
+
+    public function isAdministrator()
+    {
+    	return in_array($this->currentUserPermissionLevel, $this->permissionLevels['administrator']);
+    }
+
+    public function isEditor()
+    {
+    	return in_array($this->currentUserPermissionLevel, $this->permissionLevels['editor']);
+    }
+
+    public function isViewer()
+    {
+    	return in_array($this->currentUserPermissionLevel, $this->permissionLevels['viewer']);
     }
 }
