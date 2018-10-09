235508a860555d36c239699ed6f4cffda6b5e60f
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-10-09 16:57:28 +1100

    Added a few more users to test pagination

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,15 +14,61 @@ class UsersTableSeeder extends Seeder
     {
     	// Set up default admin user.
         DB::table('users')->insert([
-        	'first_name' => env('SEEDER_USER_FIRST_NAME'),
-            'last_name' => env('SEEDER_USER_LAST_NAME'),
-            'name' => env('SEEDER_USER_DISPLAY_NAME'),
-            'email' => env('SEEDER_USER_EMAIL'),
-            'password' => Hash::make((env('SEEDER_USER_PASSWORD'))),
-            'user_permissions_fk' => 1,
-            'status' => 1,
-        	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        	'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            [
+            	'first_name' => env('SEEDER_USER_FIRST_NAME'),
+                'last_name' => env('SEEDER_USER_LAST_NAME'),
+                'name' => env('SEEDER_USER_DISPLAY_NAME'),
+                'email' => env('SEEDER_USER_EMAIL'),
+                'password' => Hash::make((env('SEEDER_USER_PASSWORD'))),
+                'user_permissions_fk' => 1,
+                'status' => 1,
+            	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            	'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'User 1',
+                'name' => 'Test User 1',
+                'email' => 'test1@siteshow.test',
+                'password' => Hash::make('test1'),
+                'user_permissions_fk' => 3,
+                'status' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'User 2',
+                'name' => 'Test User 2',
+                'email' => 'test2@siteshow.test',
+                'password' => Hash::make('test2'),
+                'user_permissions_fk' => 3,
+                'status' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'User 3',
+                'name' => 'Test User 3',
+                'email' => 'test3@siteshow.test',
+                'password' => Hash::make('test3'),
+                'user_permissions_fk' => 3,
+                'status' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'first_name' => 'Test',
+                'last_name' => 'User 4',
+                'name' => 'Test User 4',
+                'email' => 'test4@siteshow.test',
+                'password' => Hash::make('test4'),
+                'user_permissions_fk' => 3,
+                'status' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
         ]);
     }
 }
